83ecc5ddfe05dc18f9387f6d91b5289bdd3e940c
Simon Birrell <simon@eurodemand.com>
2019-11-11 17:53:44 +0000

    Updates

---
+++ precache-manifest.189f7468e7c554b81af3147bc0912247.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0240dd985b455dc61a1bae7cc3ea4781",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "d645643333f121fe597c",
+    "url": "/monsters-rolodex/static/css/main.9fcf8eb8.chunk.css"
+  },
+  {
+    "revision": "c4de08c8e1140d25c0ae",
+    "url": "/monsters-rolodex/static/js/2.5905bb17.chunk.js"
+  },
+  {
+    "revision": "d645643333f121fe597c",
+    "url": "/monsters-rolodex/static/js/main.40ed191c.chunk.js"
+  },
+  {
+    "revision": "2dc9b40a277f725e4d87",
+    "url": "/monsters-rolodex/static/js/runtime-main.b05ca383.js"
+  }
+]);
